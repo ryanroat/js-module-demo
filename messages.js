96d7c26d7d29a271ab0205385ea2d400fac02230
Ryan Roat <ryanroat@gmail.com>
2021-05-27 14:32:21 -0400

    export module

---
+++ messages.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+import chalk from 'chalk';
+
+const error = (msg) => {
+  console.log(chalk.red(msg));
+};
+
+const success = (msg) => {
+  console.log(chalk.green(msg));
+};
+
+module.exports = {
+  error,
+  success,
+};
